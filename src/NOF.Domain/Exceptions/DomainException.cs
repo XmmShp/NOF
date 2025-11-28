07a87b8878efa6325ccbb18ff06adfa34d41a8d1
Shoming <shoming@zju.edu.cn>
2025-11-28 18:12:10 +0800

    fix: move Failure to Domain layer
    release v1.0.2 at the same time, for emergency hotfix

--- src/NOF.Domain/Exceptions/DomainException.cs
+++ src/NOF.Domain/Exceptions/DomainException.cs
@@ -3,6 +3,9 @@ namespace NOF;
 public class DomainException : Exception
 {
     public int ErrorCode { get; }
+    public DomainException(Failure failure) : this(failure.ErrorCode, failure.Message)
+    {
+    }
     public DomainException(int errorCode, string message) : base(message)
     {
         ErrorCode = errorCode;
