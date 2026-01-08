f71928b4496dbea87e1fa62551219e1c7a1347ee
Shoming <shoming@zju.edu.cn>
2026-01-08 20:31:36 +0800

    fix: add Redis distributed cache support and fix WebUI project dependencies
    Add Microsoft.Extensions.Caching.StackExchangeRedis package reference to NOF.Sample project and configure Redis cache using connection string from configuration. Remove Microsoft.AspNetCore.Components.Web package reference from NOF.Sample.WebUI and add FrameworkReference to Microsoft.AspNetCore.App instead. Add missing Microsoft.AspNetCore.Components using directive to WebUI _Imports.razor.

--- sample/NOF.Sample/Program.cs
+++ sample/NOF.Sample/Program.cs
@@ -25,6 +25,11 @@ builder.Services.AddAntDesign()
     .AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("redis");
+});
+
 // Here, we self-call for test
 builder.Services.AddHttpClient<INOFService, NOFServiceClient>(client => client.BaseAddress = new Uri("http://localhost:55892/"));
 
